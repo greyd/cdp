f74afe9e3a2d6a80bfb5845e7b4fa98aa9e5d4ab
Valerii Pavlov <pavlov.valerii@gmail.com>
2015-03-21 12:53:20 +0200

    Install nessesary utils and nodejs

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,2 +1,6 @@
-# Install htop
-sudo apt-get install htop -y
+# Install useful utils
+sudo apt-get install htop curl git build-essential -y
+
+# Install Node.js
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install nodejs build-essential -y
