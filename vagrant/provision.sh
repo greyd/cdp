aa6d418b3b7ce22279555d2625dbedece7571f9e
Valerii Pavlov <pavlov.valerii@gmail.com>
2015-03-21 16:51:13 +0200

    Add mongo db to the provision script

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
 # Install useful utils
-sudo apt-get install htop curl git build-essential -y
+sudo apt-get update
+sudo apt-get install htop curl git python-software-properties python g++ make -y
 
 # Install Node.js
-curl -sL https://deb.nodesource.com/setup | sudo bash -
-sudo apt-get install nodejs build-essential -y
+sudo add-apt-repository ppa:chris-lea/node.js -y
+sudo apt-get update
+sudo apt-get install nodejs -y
+echo prefix = ~/.node >> ~/.npmrc
+export PATH="$PATH:$HOME/.node/bin"
+
+# Install mongoDb
+Sudo apt-get insdtall mongodb-server mongodb-clients -y
