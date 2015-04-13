52ef28e6934897cfc9ce0a086aa5fdd4e4365bef
Valerii Pavlov <pavlov.valerii@gmail.com>
2015-04-13 14:05:59 +0300

    MOde router to a separate folder

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var router = require('./routers/index');
 var server = express();
 
 server.get('/', function (req, res) {
     res.end('Hello world!!!');
 });
 
+server.use('/api', router);
 module.exports = server;
